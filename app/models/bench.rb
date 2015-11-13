a166358570d377ad6d141c85bfa53e3284ea6a9b
Alex Harris <chasenyc@gmail.com>
2015-11-13 13:45:58 -0500

    Refactor bench.in_bounds to not query entire db.

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -4,18 +4,7 @@ class Bench < ActiveRecord::Base
     bounds = JSON.parse(bounds)
     top_right = bounds['northEast']
     bottom_left = bounds['southWest']
-    results = []
-    benches = Bench.all
-
-    benches.each do |bench|
-      if (bench.lat > bottom_left['lat'] && bench.lat < top_right['lat'] &&
-          bench.lng > bottom_left['lng'] && bench.lng < top_right['lng']) then
-        results.push(bench)
-      end
-
-    end
-
-    return results
+    Bench.where("lat > #{bottom_left['lat']} AND lat < #{top_right['lat']}").   where("lng > #{bottom_left['lng']} AND lng < #{top_right['lng']}")
   end
 
 end
